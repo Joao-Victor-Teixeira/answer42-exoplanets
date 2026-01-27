28647017a76645af51fba8bd132cfd226fa97be0
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-27 15:37:44 -0300

    create api

---
+++ answer42-api/src/main/java/com/joaodev/answer42_api/Answer42ApiApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.answer42_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Answer42ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Answer42ApiApplication.class, args);
+	}
+
+}
---
+++ answer42-api/src/test/java/com/joaodev/answer42_api/Answer42ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.answer42_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Answer42ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
