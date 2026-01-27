069c143c80e7ae9be3ca1280c469dc6562145957
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-27 09:56:29 -0300

    Created project

---
+++ answer42-exoplanets/src/main/java/com/joaodev/answer42_exoplanets/Answer42ExoplanetsApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.answer42_exoplanets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Answer42ExoplanetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Answer42ExoplanetsApplication.class, args);
+	}
+
+}
---
+++ answer42-exoplanets/src/test/java/com/joaodev/answer42_exoplanets/Answer42ExoplanetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.answer42_exoplanets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Answer42ExoplanetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
